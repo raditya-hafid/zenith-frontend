ac055cbef5235dfaeeff3181441f23c284434542
raditya-hafid <radityahafid123@gmail.com>
2025-11-05 15:34:46 +0700

    err

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+// 1. IMPORT halaman Register Anda
+import RegisterView from '../views/Register.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: HomeView
+    },
+    // 2. TAMBAHKAN rute ini
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterView
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+      // Rute ini biarkan saja
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
 })
 
 export default router
